18214cfb63e08fe57a0c04c207de893f715b2230
Jorge Lázaro Ruiz <jorgelazaro2002@hotmail.com>
2023-06-14 01:20:26 +0200

    characters WIP

--- characters.py
+++ characters.py
@@ -8,3 +8,21 @@ class Trainer:
         """Añade una habilidad al entrenador con el modificador indicado."""
         self.abilities[ability] = modifier
 
+
+class Pokemon:
+    def __init__(self, name):
+        self.name = name
+        self.moves = {}
+        self.physical_stress = 0
+        self.mental_stress = 0
+        self.max_mental_stress = 2
+        self.max_physical_stress = 2
+        self.consequences = {}
+
+    def add_move(self, move: str, modifier: int):
+        """Añade un movimiento al Pokémon con el modificador indicado."""
+        self.moves[move] = modifier
+
+    def add_consequence(self, consequence: str, modifier: int):
+        """Añade una consecuencia al Pokémon con el modificador indicado."""
+        self.consequences[consequence] = modifier
